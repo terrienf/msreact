82eb8247af24338ac59d6f1383553f9b6d951367
François TERRIEN <francois.terrien2022@campus-eni.fr>
2023-06-07 17:01:49 +0200

    7/6 à 17h

--- src/pages/Admin/User/UserAdd.js
+++ src/pages/Admin/User/UserAdd.js
@@ -1,29 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {userService} from "../../../services/user.service";
-
+import { userService } from "../../../services/user.service";
 
 const UserAdd = () => {
-    const [user, setUser] = useState([])
-    let navigate = useNavigate()
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        roles: ['ROLE_USER'],
+    });
+    const navigate = useNavigate();
 
     // Gestion de la modification des champs du formulaire
     const onChange = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        })
-        e.preventDefault()
-    }
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        if (e.target.name === 'roles') {
+            const roles = value ? ['ROLE_ADMIN'] : ['ROLE_USER'];
+            setUser({
+                ...user,
+                roles: roles,
+            });
+        } else {
+            setUser({
+                ...user,
+                [e.target.name]: value,
+            });
+        }
+    };
 
     // Gestion de la soumission du formulaire
     const onSubmit = (e) => {
-        e.preventDefault()
-        console.log(user)
+        e.preventDefault();
+        console.log(user);
         userService.addUser(user)
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-    }
+            .then(res => {
+                console.log(res);
+                navigate('/admin/user/index', { replace: true });
+            })
+            .catch(err => console.log(err));
+    };
 
     return (
         <div className="UserEdit">
@@ -38,6 +52,19 @@ const UserAdd = () => {
                     <label htmlFor="password">Mot de passe</label>
                     <input type="password" name="password" onChange={onChange} />
                 </div>
+
+                <div className="group">
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="roles"
+                            checked={user.roles.includes('ROLE_ADMIN')}
+                            onChange={onChange}
+                        />
+                        Administrateur
+                    </label>
+                </div>
+
                 <div className="group">
                     <button>Ajouter</button>
                 </div>
